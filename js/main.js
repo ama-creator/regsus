2cc2e60bf680d2a7d5ce71cea75bef6a9e32599f
Markevich Anatoly <markduho0@gmail.com>
2020-12-07 09:24:07 +0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,64 @@
+
+
+//  scroll reveal
+window.sr = ScrollReveal();
+
+sr.reveal('.animate-left', {
+  origin: 'left',
+  duration: 1000,
+  distance: '25em',
+  delay: 600
+});
+
+sr.reveal('.animate-right', {
+  origin: 'right',
+  duration: 1000,
+  distance: '25em',
+  delay: 900
+});
+
+sr.reveal('.animate-top', {
+  origin: 'top',
+  duration: 1000,
+  distance: '25em',
+  delay: 300
+});
+
+sr.reveal('.animate-bottom', {
+  origin: 'bottom',
+  duration: 1000,
+  distance: '25em',
+  delay: 600
+});
+
+
+
+
+$(function(){
+   
+
+  $('.menu__btn').on('click', function () {
+
+  $('.menu__btn').toggleClass('menu__btn--active');
+
+   $('.header-menu__item').toggleClass('menu__list--active');
+
+   $('.header-box__contacts-number').toggleClass('menu__list--active');
+
+   $('.header-box__contacts-info').toggleClass('menu__list--active');
+
+  })
+ 
+
+ $('.header-menu__link').on('click', function () {
+
+  $('.menu__btn').removeClass('menu__btn--active');
+
+   $('.header-menu__list', '.header-box__contacts-number', '.header-box__contacts-info').removeClass('menu__list--active');
+   
+
+  })
+ 
+
+});
+ 
